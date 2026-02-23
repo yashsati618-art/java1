3f039728519396d449cd9a6c8da5066d701c0568
yashsati618-art <yashsati618@gmail.com>
2026-02-23 20:10:50 +0530

    Complete UC2 OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,11 @@
-/**
- * OOPSBannerApp - UC1: Printing Literal Text.
- * Foundational step for the Banner Application project.
- *
- * @author Yash Sati
- * @version 1.0
- */
 public class OOPSBannerApp {
-
-    /**
-     * Entry point of the application.
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
-        // Core Logic: Invoking the println method with a String Literal
-        System.out.println("OOPS");
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
     }
 }
