2d4a19ecbdc6de3102a3b0af21c4ccf23ccaba9b
yashsati618-art <yashsati618@gmail.com>
2026-02-23 20:23:41 +0530

    UC4: Refactored with String array and enhanced for-loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,17 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String line1 = String.join("", "  *** ", "  *** ", " **** ", " **** ");
-        String line2 = String.join("", " * * ", "* * ", "* * ", "* ");
-        String line3 = String.join("", " * * ", "* * ", "* * ", "* ");
-        String line4 = String.join("", " * * ", "* * ", "**** ", " *** ");
-        String line5 = String.join("", " * * ", "* * ", "* ", "    * ");
-        String line6 = String.join("", " * * ", "* * ", "* ", "    * ");
-        String line7 = String.join("", "  *** ", "  *** ", "* ", " **** ");
+        String[] banner = new String[7];
 
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
-        System.out.println(line4);
-        System.out.println(line5);
-        System.out.println(line6);
-        System.out.println(line7);
+        banner[0] = String.join("", "  *** ", "  *** ", " **** ", " **** ");
+        banner[1] = String.join("", " * * ", "* * ", "* * ", "* ");
+        banner[2] = String.join("", " * * ", "* * ", "* * ", "* ");
+        banner[3] = String.join("", " * * ", "* * ", "**** ", " *** ");
+        banner[4] = String.join("", " * * ", "* * ", "* ", "    * ");
+        banner[5] = String.join("", " * * ", "* * ", "* ", "    * ");
+        banner[6] = String.join("", "  *** ", "  *** ", "* ", " **** ");
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
