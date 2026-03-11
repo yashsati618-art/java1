e61c48bbe575d891091aab9c96eae3fc2730112c
yashsati618-art <yashsati618@gmail.com>
2026-03-11 09:27:52 +0530

    chore: save progress on OOPSBannerApp before merge

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,13 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] banner = new String[7];
-
-        banner[0] = String.join("", "  *** ", "  *** ", " **** ", " **** ");
-        banner[1] = String.join("", " * * ", "* * ", "* * ", "* ");
-        banner[2] = String.join("", " * * ", "* * ", "* * ", "* ");
-        banner[3] = String.join("", " * * ", "* * ", "**** ", " *** ");
-        banner[4] = String.join("", " * * ", "* * ", "* ", "    * ");
-        banner[5] = String.join("", " * * ", "* * ", "* ", "    * ");
-        banner[6] = String.join("", "  *** ", "  *** ", "* ", " **** ");
+        // UC5: Streamlined Array Initialization using String.join()
+        String[] banner = {
+            String.join(" ", "  OOO  ", "  OOO  ", " PPPP  ", "  SSS  "),
+            String.join(" ", " O   O ", " O   O ", " P   P ", " S     "),
+            String.join(" ", " O   O ", " O   O ", " PPPP  ", "  SSS  "),
+            String.join(" ", " O   O ", " O   O ", " P     ", "     S "),
+            String.join(" ", "  OOO  ", "  OOO  ", " P     ", "  SSS  ")
+        };
 
         for (String line : banner) {
             System.out.println(line);
